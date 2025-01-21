862a65edca4c5021f75e9cbdd82f4bdf2d2633ab
Harrison Liddiard <omniaura5@gmail.com>
2025-01-21 14:22:56 -0500

    add missing aria role

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App() {
     return (
       <p className={cn(s.instructions, { [s.copied]: copied })}>
         {copied ? (
-          <div className="animate__animated animate__bounceIn">
+          <div className="animate__animated animate__bounceIn" role="alert">
             <span className={s.emoji}>{emojis[copiedIndex]}</span> copied to
             clipboard
           </div>
